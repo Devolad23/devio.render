d6d3c8b32b74cc2d6b036fca29c56f7ac8fe27e7
Devolad23 <devil555@outlook.sa>
2025-09-14 15:37:25 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+{
+  "isActive": true,
+  "adminUsers": [
+    "1359245683936329788",
+    "1279737263110230182"
+  ],
+  "lastActivity": "2025-08-13T23:02:51.368Z",
+  "statistics": {
+    "totalBroadcasts": 6,
+    "totalMessagesSent": 436,
+    "failedMessages": 14,
+    "usersManaged": 2
+  }
+}
